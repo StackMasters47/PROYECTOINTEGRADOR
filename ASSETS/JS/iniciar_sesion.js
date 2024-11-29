6e7eba88f841b468a53ba182fd808774c418036e
HannMejia <hmargarita.mejiar@gmail.com>
2024-11-29 08:05:37 -0600

    Subo versión final de diseño de formularios iniciar_sesion y recuperar_contraseña

--- ASSETS/JS/iniciar_sesion.js
+++ ASSETS/JS/iniciar_sesion.js
@@ -1,28 +1,24 @@
-const usuarioPrueba = {
-  email: "testuser@gmail.com",
-  password: "12345"
-};
+const iniciarSesion = document.querySelector('#iniciarSesion') //para seleccionar el elemento con id específico, en este caso, el form con id "registro"
+iniciarSesion.addEventListener('submit', (event) => { //para atender el evento "submit" y ejecutar una función 
+    event.preventDefault(); //previene la recarga automática de la página
 
-const usuarioPrueba2 = {
-  email: "testuser2@gmail.com",
-  password: "54321"
-}
+    //Obtener datos del usuario a traves de las entradas con los ids especificados
+    const email = document.querySelector('#emailInput').value.trim();
+    const password = document.querySelector('#pass1Input').value.trim();
 
-localStorage.setItem("usuario", JSON.stringify(usuarioPrueba));
-localStorage.setItem("usuario2", JSON.stringify(usuarioPrueba2));
+    // Para obtener los datos de local storage, y convertirlos de formato JSON a un array de objetos; si no hay datos en local storage, devuelve un array vacio
+    const Users = JSON.parse(localStorage.getItem('users')) || [];
+    // Para validar datos: comparar los datos obtenidos de local storage con los datos ingresados por el usuario; ambos condiciones deben cumplirse para continuar
+    const validUser = Users.find(user => user.email === email && user.password === password);
 
-function validateUser(event) {
-  event.preventDefault();
+    //Si el usuario no es valido, retorna una alerta con este mensaje
+    if(!validUser){
+        return alert('Usuario y/o contraseña incorrectos')
+    }
 
-  const userEmail = document.getElementById("userEmail").value.trim();
-  const password = document.getElementById("password").value.trim();
-  
-  const storedUser1 = JSON.parse(localStorage.getItem("usuario"));
-  const storedUser2 = JSON.parse(localStorage.getItem("usuario2"));
+    //Si los datos son validos, retorna una alerta con el mensaje y redirige a la página de inicio
+    alert(`Bienvenido ${validUser.name}`)
+    localStorage.setItem('login_success', JSON.stringify(validUser))
+    window.location.href = 'index.html'   
 
-  if ((storedUser1 && userEmail === storedUser1.email && password === storedUser1.password) || (storedUser2 && userEmail === storedUser2.email && password === storedUser2.password)) { 
-    window.location.href = "../index.html"; 
-  } else { 
-    document.getElementById("errorMessage").textContent = "Nombre de usuario o contraseña inválidos"; 
-  }
-}
+});
