8f974f9acc007fbdcf4389c151dc5a0fc7931fc2
HannMejia <hmargarita.mejiar@gmail.com>
2024-12-01 18:03:27 -0600

    subo version final de formularios iniciar sesion / registro / recuperar contraseña y archivo adicional de términos y condiciones

--- ASSETS/JS/iniciar_sesion.js
+++ ASSETS/JS/iniciar_sesion.js
@@ -1,24 +1,44 @@
 const iniciarSesion = document.querySelector('#iniciarSesion') //para seleccionar el elemento con id específico, en este caso, el form con id "registro"
-iniciarSesion.addEventListener('submit', (event) => { //para atender el evento "submit" y ejecutar una función 
+iniciarSesion.addEventListener("submit", (event) => { //para atender el evento "submit" y ejecutar una función 
     event.preventDefault(); //previene la recarga automática de la página
 
     //Obtener datos del usuario a traves de las entradas con los ids especificados
-    const email = document.querySelector('#emailInput').value.trim();
-    const password = document.querySelector('#pass1Input').value.trim();
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password1");
+    //const alertContainer = document.getElementById("alertContainer");
 
-    // Para obtener los datos de local storage, y convertirlos de formato JSON a un array de objetos; si no hay datos en local storage, devuelve un array vacio
+    let valido = true;
+
+    if (email.value.trim() === "") {
+        email.classList.add("is-invalid");
+        valido = false;
+    } else {
+        email.classList.remove("is-invalid");
+        email.classList.add("is-valid");
+    }
+
+    if (password.value.trim() === "") {
+        password.classList.add("is-invalid");
+        valido = false;
+    } else {
+        password.classList.remove("is-invalid");
+        password.classList.add("is-valid");
+    }
+
+    if(valido) {
+       // Para obtener los datos de local storage, y convertirlos de formato JSON a un array de objetos; si no hay datos en local storage, devuelve un array vacio
     const Users = JSON.parse(localStorage.getItem('users')) || [];
     // Para validar datos: comparar los datos obtenidos de local storage con los datos ingresados por el usuario; ambos condiciones deben cumplirse para continuar
-    const validUser = Users.find(user => user.email === email && user.password === password);
+    const validUser = Users.find(user => user.email === email.value.trim() && user.password === password.value.trim());
 
     //Si el usuario no es valido, retorna una alerta con este mensaje
     if(!validUser){
-        return alert('Usuario y/o contraseña incorrectos')
+        return alert('Usuario y/o contraseña incorrectos');
     }
 
     //Si los datos son validos, retorna una alerta con el mensaje y redirige a la página de inicio
-    alert(`Bienvenido ${validUser.name}`)
-    localStorage.setItem('login_success', JSON.stringify(validUser))
-    window.location.href = 'index.html'   
-
+    alert(`Bienvenido ${validUser.nombre}`);
+    localStorage.setItem('login_success', JSON.stringify(validUser));
+    window.location.href = '/index.html'; 
+    }
 });
